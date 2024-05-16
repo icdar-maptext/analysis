21883d492d6c4bb1df222be7843f70271d2a95a0
Joseph Chazalon <joseph.chazalon@lrde.epita.fr>
2024-05-16 12:00:27 +0200

    Quick backup of date logic before removing it

--- icdar24_maptext_analysis/loaders.py
+++ icdar24_maptext_analysis/loaders.py
@@ -9,6 +9,8 @@ import glob
 from typing import Literal
 import json
 
+import pandas as pd
+
 from .paths import RELPATH_DIR_IMAGES, RELPATH_DIR_RESULTS, RELPATH_DIR_SUBMISSIONS, RELPATH_DIR_GT
 
 # Some utility fonctions to obtain file lists
@@ -29,6 +31,7 @@ DATASET_IDTONAME: dict[int, TYPE_DATASET_NAME] = {
 
 @lru_cache
 def list_available_images(subset: TYPE_DATASET_NAME) -> list[Path]:
+    """Returns the list of available images for a given subset"""
     if subset not in VALID_SUBSETS:
         raise ValueError(f"Invalid subset name: {subset}")
     file_ext = "png" if subset == "rumsey" else "jpg"
@@ -57,6 +60,7 @@ def list_results(task: TYPE_TASK_ID, subset: TYPE_DATASET_NAME) -> list[str]:
     all_files = [Path(p) for p in sorted(all_files)]
     return [p.stem for p in all_files]
 
+
 def load_result(task: TYPE_TASK_ID, subset: TYPE_DATASET_NAME, submission_id: str) -> dict:
     """Reads the json file associated to a particular submission, for a given task and dataset.
 
@@ -76,6 +80,57 @@ def load_result(task: TYPE_TASK_ID, subset: TYPE_DATASET_NAME, submission_id: st
     file_path = RELPATH_DIR_RESULTS / f"t{task}" / f"f{subset_id}" / f"{submission_id}.json"
     with open(file_path, "r", encoding="utf-8") as f:
         return json.load(f)
+
+
+def load_results(taskid: TYPE_TASK_ID, subset: TYPE_DATASET_NAME, filter_fn=None):
+    """
+    Load all results for a given task and subset as dataframes.
+    :param taskid: The task ID.
+    :param subset: The subset.
+    :param filter: A function that filters the results by submission id.
+    :return: A list of results.
+    """
+    submission_ids = list_results(taskid, subset)
+    if filter_fn:
+        submission_ids = [s for s in submission_ids if filter_fn(s)]
+ 
+    results_global = []
+    results_images = []
+
+    for submission_id in submission_ids:
+        # Read the results
+        result_raw = load_result(taskid, subset, submission_id)
+        result_global = result_raw['results']
+        result_images = result_raw['images']
+
+        # Process global results first
+        result_global['task_id'] = taskid
+        result_global['subset'] = subset
+        result_global['submission_id'] = submission_id
+        results_global.append(result_global)
+
+        # # Add the submission ID to the results
+        for image_id, result_image in result_images.items():
+            result_image['task_id'] = taskid
+            result_image['subset'] = subset
+            result_image['submission_id'] = submission_id
+            result_image['image_id'] = image_id
+            results_images.append(result_image)
+
+   # Create two dataframes: one for the global results and one for the image results
+    results_global_df = pd.DataFrame.from_records(
+        results_global,
+        columns=['task_id', 'subset', 'submission_id',
+                 "quality", "fscore", "tightness", "precision", "recall"],
+        index=['task_id', 'subset', 'submission_id'])
+
+    results_images_df = pd.DataFrame.from_records(
+        results_images,
+        columns=['task_id', 'subset', 'submission_id', 'image_id',
+                 "quality", "fscore", "tightness", "precision", "recall"],
+        index=['task_id', 'subset', 'submission_id', 'image_id'])
+
+    return results_global_df, results_images_df
 
 @lru_cache
 def list_submissions(task: TYPE_TASK_ID, subset: TYPE_DATASET_NAME) -> list[str]:
